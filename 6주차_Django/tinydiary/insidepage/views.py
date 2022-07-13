3aa59118eb95ed4a246cbbee1e6a962de1bdbaca
nneo <aisapple1030@naver.com>
2022-07-14 00:10:27 +0900

    달별 검색 & 타이틀+본문 검색 완료

--- 6주차_Django/tinydiary/insidepage/views.py
+++ 6주차_Django/tinydiary/insidepage/views.py
@@ -41,8 +41,39 @@ def create(request):
 
 def search(request):
     query = request.GET.get('query', '')
-    if query:
-        diaries = Diary.objects.filter(title__contains=query)
-        return render(request, 'insidepage/search.html', {'diaries': diaries})
-    else:
+    month = request.GET.get('month', '')
+
+    month_val_result = validate_month(month)
+    # 1. month 에러 O, query 존재 X
+    if month_val_result and not query:
+        return render(request, 'insidepage/search.html', {'error': month_val_result['error'], 'guide': 'Enter something you want to find...'})
+    # 2. month 에러 O, query 존재 O
+    elif month_val_result and query:
+        return render(request, 'insidepage/search.html', {'error': month_val_result['error']})
+    # 3. month 에러 X, query 존재 X
+    elif not month_val_result and not query:
         return render(request, 'insidepage/search.html', {'guide': 'Enter something you want to find...'})
+
+    # 4. month 에러 X, query 존재 O
+    if month == '': # month에 아무것도 입력되지 않은 경우
+        diaries = (Diary.objects.filter(title__contains=query) | Diary.objects.filter(body__contains=query)).order_by("-pub_date")
+        return render(request, 'insidepage/search.html', {'diaries': diaries})
+    else: 
+        diaries = (Diary.objects.filter(pub_date__month=int(month), title__contains=query) | Diary.objects.filter(pub_date__month=int(month), body__contains=query)).order_by("-pub_date")
+        return render(request, 'insidepage/search.html', {'diaries': diaries})
+
+def validate_month(month):
+    # 1. 숫자가 아닌 타입이 입력된 경우
+    try:
+        month_int = int(month)
+    except ValueError: 
+        if month == '': # 1-1. 입력이 없는 경우[OK]
+            return
+        else: # 1-2. 숫자가 아닌 타입이 입력된 경우
+            return {'error': 'Only numbers must be entered in Month'}
+    
+    # 2. 1 ~ 12 사이의 숫자가 아닌 경우
+    if month_int<1 and month_int>12: # 1~12사이의 숫자가 아닌 경우
+        return {'error': 'Only numbers between 1 and 12 must be entered in Month'}
+    # 3. 1 ~ 12 사이의 숫자인 경우[OK]
+    return
